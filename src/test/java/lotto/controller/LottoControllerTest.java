3871c6bf73240252434981caa36b90508ac405f9
ParkJiYeoung8297 <wldud8297@gmail.com>
2025-11-01 00:51:17 +0900

    test(controller): 사용자에게 로또 결과 출력 테스트 추가

--- src/test/java/lotto/controller/LottoControllerTest.java
+++ src/test/java/lotto/controller/LottoControllerTest.java
@@ -2,9 +2,14 @@ package lotto.controller;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import lotto.exception.ErrorMessage;
+import lotto.service.LottoService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -12,14 +17,41 @@ class LottoControllerTest extends NsTest{
     private static final String ERROR_MESSAGE = "[ERROR]";
 
     @Test
-    void 구매금액_단순입출력_테스트() {
-        assertSimpleTest(
+    void 기능_테스트() {
+        assertRandomUniqueNumbersInRangeTest(
                 () -> {
                     run("8000", "1,2,3,4,5,6", "7");
                     assertThat(output()).contains(
-                            "구입금액을 입력해 주세요."
+                            "구입금액을 입력해 주세요.",
+                            "8개를 구매했습니다.",
+                            "[8, 21, 23, 41, 42, 43]",
+                            "[3, 5, 11, 16, 32, 38]",
+                            "[7, 11, 16, 35, 36, 44]",
+                            "[1, 8, 11, 31, 41, 42]",
+                            "[13, 14, 16, 38, 42, 45]",
+                            "[7, 11, 30, 40, 42, 43]",
+                            "[2, 13, 22, 32, 38, 45]",
+                            "[1, 3, 5, 14, 22, 45]"
+//                            "당첨 번호를 입력해 주세요.",
+//                            "보너스 번호를 입력해 주세요.",
+//                            "당첨 통계",
+//                            "---",
+//                            "3개 일치 (5,000원) - 1개",
+//                            "4개 일치 (50,000원) - 0개",
+//                            "5개 일치 (1,500,000원) - 0개",
+//                            "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
+//                            "6개 일치 (2,000,000,000원) - 0개",
+//                            "총 수익률은 62.5%입니다."
                     );
-                }
+                },
+                List.of(8, 21, 23, 41, 42, 43),
+                List.of(3, 5, 11, 16, 32, 38),
+                List.of(7, 11, 16, 35, 36, 44),
+                List.of(1, 8, 11, 31, 41, 42),
+                List.of(13, 14, 16, 38, 42, 45),
+                List.of(7, 11, 30, 40, 42, 43),
+                List.of(2, 13, 22, 32, 38, 45),
+                List.of(1, 3, 5, 14, 22, 45)
         );
     }
 
@@ -61,7 +93,8 @@ class LottoControllerTest extends NsTest{
     @Override
     public void runMain() {
         View view = new View();
-        LottoController lottoController = new LottoController(view);
+        LottoService lottoService = new LottoService();
+        LottoController lottoController = new LottoController(view, lottoService);
         lottoController.doLotto();
     }
 }
