9308167698506270c904fcc6d673c36c548c0efb
ParkJiYeoung8297 <wldud8297@gmail.com>
2025-10-31 20:14:28 +0900

    test(service): 구입한 로또 개수 계산 및 개수만큼 로또 생성 테스트 추가

---
+++ src/test/java/lotto/service/LottoServiceTest.java
@@ -0,0 +1,35 @@
+package lotto.service;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LottoServiceTest {
+
+    @Test
+    void 구입한_개수만큼_로또랜덤생성_테스트() {
+        LottoService lottoService = new LottoService();
+        assertRandomUniqueNumbersInRangeTest(
+                () -> {
+                List<List<Integer>> result = lottoService.generateRandomLottos(5000);
+                List<List<Integer>> expected = new ArrayList<>();
+                expected.add(List.of(8, 21, 23, 41, 42, 43));
+                expected.add(List.of(3, 5, 11, 16, 32, 38));
+                expected.add(List.of(7, 11, 16, 35, 36, 44));
+                expected.add(List.of(1, 8, 11, 31, 41, 42));
+                expected.add(List.of(13, 14, 16, 38, 42, 45));
+                assertThat(result).containsExactlyElementsOf(expected);
+
+                },
+                List.of(8, 21, 23, 41, 42, 43),
+                List.of(3, 5, 11, 16, 32, 38),
+                List.of(7, 11, 16, 35, 36, 44),
+                List.of(1, 8, 11, 31, 41, 42),
+                List.of(13, 14, 16, 38, 42, 45)
+        );
+    }
+
+}
