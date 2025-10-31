c81d5aba1a73febdbc8405aec11adc2dc7910457
ParkJiYeoung8297 <wldud8297@gmail.com>
2025-10-31 20:08:37 +0900

    feat (randomGenerator) : Randoms 라이브러리로 로또 생성 추가

---
+++ src/main/java/lotto/service/RandomGenerator.java
@@ -0,0 +1,16 @@
+package lotto.service;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
+public class RandomGenerator {
+    public static final int START_NUMBER = 1;
+    public static final int END_NUMBER = 45;
+    public static final int COUNT = 6;
+
+    public static List<Integer> getRandomLottoNumber(){
+        return Randoms.pickUniqueNumbersInRange(START_NUMBER, END_NUMBER, COUNT);
+    }
+
+}
