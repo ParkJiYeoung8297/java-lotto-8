b05f6d0e1ed87deb49b8fb7304a892bfd6754c31
ParkJiYeoung8297 <wldud8297@gmail.com>
2025-11-02 22:13:37 +0900

    test: 도메인 검증  테스트 추가

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -6,9 +6,16 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 class LottoTest {
+
+    @Test
+    void 로또_객체생성_테스트() {
+        assertThatCode(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6)))
+                .doesNotThrowAnyException();
+    }
+
     @Test
     void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
@@ -22,5 +29,22 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @DisplayName("로또 번호에 1-45 사이 숫자가 아니면 예외가 발생한다.")
+    @Test
+    void 로또_번호_범위밖_예외_테스트() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 78)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+
+    @DisplayName("로또 번호는 오름차순 정렬되야한다.")
+    @Test
+    void 로또_정렬_테스트() {
+        Lotto lotto = new Lotto(List.of(1, 2, 32, 4, 3, 5));
+        List<Integer> expected = lotto.getNumbers();
+        List<Integer> actual = List.of(1, 2, 3, 4, 5, 32);
+
+        assertThat(expected).containsExactlyElementsOf(actual);
+    }
+
 }
