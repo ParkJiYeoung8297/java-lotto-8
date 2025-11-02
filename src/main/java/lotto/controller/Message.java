2bdd53623b524ff9cb9de7e4a1dc5aff36d84316
ParkJiYeoung8297 <wldud8297@gmail.com>
2025-11-03 05:56:47 +0900

    chore: 줄 바꿈 추가

--- src/main/java/lotto/controller/Message.java
+++ src/main/java/lotto/controller/Message.java
@@ -2,10 +2,10 @@ package lotto.controller;
 
 public class Message {
     public static final String REQUEST_PURCHASE_PRICE = "구입금액을 입력해 주세요.";
-    public static final String PRINT_LOTTO_COUNT = "%d개를 구매했습니다.";
-    public static final String REQUEST_TARGET_LOTTO = "당첨 번호를 입력해 주세요.";
-    public static final String REQUEST_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
-    public static final String PRINT_RESULT_HEADER = "당첨 통계\n---\n";
+    public static final String PRINT_LOTTO_COUNT = "\n%d개를 구매했습니다.";
+    public static final String REQUEST_TARGET_LOTTO = "\n당첨 번호를 입력해 주세요.";
+    public static final String REQUEST_BONUS_NUMBER = "\n보너스 번호를 입력해 주세요.";
+    public static final String PRINT_RESULT_HEADER = "\n당첨 통계\n---";
     public static final String PRINT_RESULT_LOTTO = "%s - %d개";
     public static final String PRINT_LOTTO_RETURNS = "총 수익률은 %s%%입니다.";
 }
