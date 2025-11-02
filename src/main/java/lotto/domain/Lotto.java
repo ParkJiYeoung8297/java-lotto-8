de05bc941d1979f12ff84ee3fa2f511fcd9a9cbe
ParkJiYeoung8297 <wldud8297@gmail.com>
2025-11-02 22:13:15 +0900

    feat(lotto): Lotto 도메인 검증 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,21 +1,44 @@
 package lotto.domain;
 
+import lotto.exception.ErrorMessage;
+import lotto.exception.Validator;
+
 import java.util.*;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateLottoLength(numbers);
+        validateUniqueNumber(numbers);
+        validateNumberInRange(numbers);
         this.numbers = new ArrayList<>(numbers);
         Collections.sort(this.numbers);
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validateLottoLength(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+            throw new IllegalArgumentException(ErrorMessage.LOTTO_NUMBER_LENGTH_ERROR.getMessage());
         }
     }
+
+    private void validateUniqueNumber(List<Integer> numbers)  {
+        Set<Integer> nums = new HashSet<>(numbers);
+        if (nums.size() != 6) {
+            throw new IllegalArgumentException(ErrorMessage.NOT_UNIQUE_LOTTO_NUMBER.getMessage());
+        }
+    }
+
+    private void validateNumberInRange(List<Integer> numbers)  {
+        for (int num : numbers){
+            Validator.validateNotEmptyValue(Integer.toString(num));
+            if (num < 1 || num > 45){
+                throw new IllegalArgumentException(ErrorMessage.LOTTO_NUMBER_NOT_IN_RANGE.getMessage());
+            }
+        }
+    }
+
+
 
     // 숫자 비교
     public int compareLotto(List<Integer> num) {
