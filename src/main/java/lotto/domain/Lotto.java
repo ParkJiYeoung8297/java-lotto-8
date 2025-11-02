fc6c121cffc41a000a601122e27b0873e7f38fd2
ParkJiYeoung8297 <wldud8297@gmail.com>
2025-11-02 13:15:23 +0900

    feat(lotto): 사용자 로또의 당첨 여부 게산

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,6 +1,8 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -16,5 +18,23 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    // 숫자 비교
+    public int compareLotto(List<Integer> num){
+        Set<Integer> targetLotto = new HashSet<>(num);
+        Set<Integer> randomLotto = new HashSet<>(numbers);
+        targetLotto.retainAll(randomLotto);
+        return targetLotto.size();
+    }
+
+    // 보너스 번호 맞춤 여부
+    public int checkBonusNumber(Integer bonusNum){
+        if (numbers.contains(bonusNum) == true){
+            return 1;
+        }
+        return 0;
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
 }
