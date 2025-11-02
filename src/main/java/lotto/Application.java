82c39c22e21cfa761a1378dc0c68a6388d11e9b5
ParkJiYeoung8297 <wldud8297@gmail.com>
2025-11-02 15:20:06 +0900

    feat(application): 메인 실행 함수 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,14 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.controller.View;
+import lotto.service.LottoService;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        View view = new View();
+        LottoService lottoService = new LottoService();
+        LottoController lottoController = new LottoController(view, lottoService);
+        lottoController.doLotto();
     }
 }
